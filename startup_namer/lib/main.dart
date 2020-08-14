774aa53ffc8665c3bfd5409278001d576fa6ece5
Khoa Vo <khoa@saber.vn>
2020-08-14 12:37:04 +0700

    Complete Part 2

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -8,8 +8,10 @@ class StartupNamer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: RandomWords(),
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(primaryColor: Colors.white),
+      home: const RandomWords(),
     );
   }
 }
@@ -23,6 +25,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _savedNames = <WordPair>{};
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   Widget _buildSuggestions() {
@@ -42,15 +45,51 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedNames.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
-    );
+        title: Text(pair.asPascalCase, style: _biggerFont),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedNames.remove(pair);
+            } else {
+              _savedNames.add(pair);
+            }
+          });
+        });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (context) {
+        final tiles = _savedNames.map((name) {
+          return ListTile(title: Text(name.asPascalCase, style: _biggerFont));
+        });
+        final dividedTiles = ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList();
+        return Scaffold(
+          appBar: AppBar(title: const Text('Saved Suggestions')),
+          body: ListView(children: dividedTiles),
+        );
+      },
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Startup Name Generator')),
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
       body: _buildSuggestions(),
     );
   }
